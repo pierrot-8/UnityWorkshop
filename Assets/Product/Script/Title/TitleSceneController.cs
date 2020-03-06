b7c34d14acc1f4ce96ca6444a9738a88a5cae22c
Douya_Masayuki <douya@necknecco.co.jp>
2020-03-06 21:26:15 +0900

    簡単なシーン遷移だけ

---
+++ Assets/Product/Script/Title/TitleSceneController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleSceneController : MonoBehaviour
+{
+    [SerializeField]
+    TitleSceneView titelView;
+
+    void Start()
+    {
+        titelView.Initialized();
+    }
+}
---
+++ Assets/Product/Script/Title/TitleSceneView.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using UnityEngine.SceneManagement;
+
+public class TitleSceneView : MonoBehaviour
+{
+    [SerializeField]
+    Image backGround;
+
+    [SerializeField]
+    Button tapButton;
+
+    public void Initialized()
+    {
+        tapButton.onClick.AsObservable().Subscribe(a => SceneManager.LoadScene("SelectScene")).AddTo(this);
+    }
+}
