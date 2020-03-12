085a92c3457231edd9e5195bd74d84f888dc40c8
Douya_Masayuki <great.pierrot.do8@gmail.com>
2020-03-12 19:26:35 +0900

    新規クラスの追加 枠組みだけ

---
+++ Assets/Product/Script/Cube/CubeSceneController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WorkShop.Cube
+{
+    public class CubeSceneController : MonoBehaviour
+    {
+        [SerializeField]
+        CubeSceneView view;
+
+        void Start()
+        {
+            var viewModel = new CubeSceneViewModel();
+            view.Initialized(viewModel);
+        }
+
+    }
+}
---
+++ Assets/Product/Script/Cube/CubeSceneView.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WorkShop.Cube
+{
+    public class CubeSceneView : MonoBehaviour
+    {
+        CubeSceneViewModel viewModel;
+
+        public void Initialized(CubeSceneViewModel viewModel)
+        {
+            this.viewModel = viewModel;
+        }
+    }
+}
